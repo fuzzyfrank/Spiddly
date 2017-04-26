488475f334fc8cd0afe32fc2d510b5c9a33cfe0f
cablooshe <cablooshe@gmail.com>
2017-04-25 20:06:29 -0400

    made another comment for testing

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		testRing.generateWeapon();
 		Shield testShield = new Shield();
 		testShield.generateWeapon();
-		//This is a comment.  hi from gordon
+		//This is a comment.  hi from gordon Hello again
 	}
 	
 }
